8e87568e94031d40044a95aeddd5d928e8b08b3e
Mendy <mendy@talenttribe.me>
2016-10-28 02:25:23 +0300

    db fixes

--- server.js
+++ server.js
@@ -2,25 +2,25 @@
 // get the packages we need ============
 // =======================
 var express     = require('express');
-var app         = express();
 var bodyParser  = require('body-parser');
+var app         = express();
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User   = require('./app/models/user'); // get our mongoose model
-var Api = require('./login-api-new');
+
 
 // =======================
 // configuration =========
 // =======================
 var port = process.env.PORT || 4000; // used to create, sign, and verify tokens
-//mongoose.connect('mongodb://mendy:shme3184@ds161315.mlab.com:61315/mendy-edri-db'); // connect to database
+mongoose.connect('mongodb://mendy:shme3184@ds161315.mlab.com:61315/mendy-edri-db'); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 // use morgan to log requests to the console
@@ -39,6 +39,7 @@ var allowCrossDomain = function(req, res, next) {
 }
 
 app.use(allowCrossDomain);
+//apiRoutes.use(allowCrossDomain);
 
 // API ROUTES -------------------
 // get an instance of the router for api routes
@@ -50,10 +51,6 @@ var apiRoutes = express.Router();
 // basic route
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
-
-    // Api("mendy@talenttribe.com", "102030").then((data) => {
-    //   console.log('fucking data: ' + JSON.stringify(data));
-    // });
 });
 
 // route to show a random message (GET http://localhost:8080/api/)
@@ -62,6 +59,17 @@ apiRoutes.get('/api', function(req, res) {
 });
 
 app.get('/api/test', function(req, res) {
+  var newUser = new User({
+    email: "mendy@talenttribe.me",
+    password: "102030",
+    admin: true
+  });
+
+  newUser.save(function(err) {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
   var data = { message: 'Welcome to the coolest API on earth!' };
   console.log('data: ' + JSON.stringify(data));
   res.json(data);
@@ -155,7 +163,7 @@ apiRoutes.get('/users', function(req, res) {
 });
 
 // apply the routes to our application with the prefix /api
-//app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 
 // API ROUTES -------------------
 // we'll get to these in a second
@@ -163,5 +171,9 @@ apiRoutes.get('/users', function(req, res) {
 // =======================
 // start the server ======
 // =======================
-app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+conn.once('open', function() {
+  app.listen(port);
+  console.log('DB is ready! http://localhost:' + port);
+});
