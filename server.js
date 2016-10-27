bcd7838fba431be16799d83bef899cf7a6aa96ac
Mendy <mendy@talenttribe.me>
2016-10-28 02:29:56 +0300

    db bug

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var User   = require('./app/models/user'); // get our mongoose model
 // configuration =========
 // =======================
 var port = process.env.PORT || 4000; // used to create, sign, and verify tokens
-mongoose.connect(' mongodb://heroku_kc5krpgj:5tbspvggrtia5h8adh2eln4r8n@ds015760.mlab.com:15760/heroku_kc5krpgj'); // connect to database
+mongoose.connect('mongodb://heroku_kc5krpgj:5tbspvggrtia5h8adh2eln4r8n@ds015760.mlab.com:15760/heroku_kc5krpgj'); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
