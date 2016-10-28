f9c76d9553a6d99855527ce08ec7cec84f628db3
Mendy <mendy@talenttribe.me>
2016-10-28 11:40:09 +0300

    db

--- server.js
+++ server.js
@@ -26,15 +26,23 @@ app.use(bodyParser.json());
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain);
+
 
 
 //app.use(allowCrossDomain);
-//apiRoutes.use(allowCrossDomain);
 
 // API ROUTES -------------------
 // get an instance of the router for api routes
 var apiRoutes = express.Router();
-
+apiRoutes.use(allowCrossDomain);
 // =======================
 // routes ================
 // =======================
