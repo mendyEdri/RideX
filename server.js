78eb8235cc47a6722e176901b957fd3828d949d7
Mendy <mendy@talenttribe.me>
2016-10-28 11:41:35 +0300

    allow origin

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ app.use(morgan('dev'));
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
     next();
 }
 app.use(allowCrossDomain);
