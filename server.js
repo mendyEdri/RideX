7a72a3d754cf9487dd9ecc077e58ad77372ad200
Mendy <mendy@talenttribe.me>
2016-11-02 01:36:00 +0200

    validate token

--- server.js
+++ server.js
@@ -140,7 +140,6 @@ apiRoutes.use(function(req, res, next) {
       });
     } else {
       return res.status(403).send({success: false, message: 'No Token Provided'});
-      //return res.json({success: false, message: 'Failed to authenticate token.'});
     }
 });
 
@@ -150,6 +149,24 @@ apiRoutes.get('/users', function(req, res) {
     res.json(users);
   });
 });
+
+apiRoutes.get('/validate', function(req, res) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+        if (err) {
+          res.json({success: false, message: 'Failed to authenticate token.'});
+        } else {
+            // if everything is good, save to request for use in other routes
+            req.decoded = decoded;
+            next();
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, message: 'No Token Provided'});
+  }
+});
+
 
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
