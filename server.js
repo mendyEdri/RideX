1338aaf1e8170d1d187c068af6d4377d559fb2f6
Mendy <mendy@talenttribe.me>
2016-11-02 01:52:15 +0200

    validate token 4

--- server.js
+++ server.js
@@ -128,6 +128,7 @@ app.post('/login', function(req, res) {
 // route middleware to verify a token
 apiRoutes.use(function(req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    console.log('validate: %%%%%%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$' + token);
     if (token) {
       jwt.verify(token, app.get('superSecret'), function(err, decoded) {
           if (err) {
@@ -149,25 +150,6 @@ apiRoutes.get('/users', function(req, res) {
     res.json(users);
   });
 });
-
-apiRoutes.get('/validate', function(req, res) {
-  console.log('validate: %%%%%%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$' + token);
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (token) {
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-        if (err) {
-          res.json({success: false, message: 'Failed to authenticate token.'});
-        } else {
-            // if everything is good, save to request for use in other routes
-            req.decoded = decoded;
-            next();
-        }
-    });
-  } else {
-    return res.status(403).send({success: false, message: 'No Token Provided'});
-  }
-});
-
 
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
