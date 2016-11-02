d55a292907f0ea08303d05ce23f95585dec819c1
Mendy <mendy@talenttribe.me>
2016-11-02 09:17:34 +0200

    validate token 6

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.post('/login', function(req, res) {
 // route middleware to verify a token
 apiRoutes.use(function(req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    console.log('validate: ' + JSON.stringify(req.body));
+    console.log('validate: ' + JSON.stringify(token));
     if (token) {
       jwt.verify(token, app.get('superSecret'), function(err, decoded) {
           if (err) {
