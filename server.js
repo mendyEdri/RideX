d73959445dd5c85c61e99c318f7fc14abf77d498
Mendy <mendy@talenttribe.me>
2016-11-05 23:48:32 +0200

    twilo 3

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/send', function(req, res) {
 
   client.messages.create({
       body: "Hey, i need a taxi at this place: ..",
-      to: "+14108675309",
+      to: "+12406075476",
       from: "+15005550006"
   }, function(err, sms) {
       process.stdout.write(sms.sid);
