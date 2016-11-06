d9e620a89a6ae7b57482e9d4ffd6f31da93c1762
Mendy <mendy@talenttribe.me>
2016-11-06 02:13:07 +0200

    twilo 11

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/send', function(req, res) {
   });
 });
 
-app.get('/sms/replay/in', function(req, res) {
+app.get('/sms', function(req, res) {
   console.log('sms: ' + req);
   res.json({ message: req });
 });
