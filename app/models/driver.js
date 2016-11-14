3f523c791b872ff3067f690aa560920ee471ac87
Mendy <mendy@talenttribe.me>
2016-11-14 09:42:14 +0200

    location in driver module 2

--- app/models/driver.js
+++ app/models/driver.js
@@ -4,12 +4,11 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('Driver', new Schema({
-    driverId: ObjectId,
     phoneNumber: String,
     loc: {
       type: [Number],  // [<longitude>, <latitude>]
       index: '2d'      // create the geospatial index
-    }
+    },
     startDate: Date,
     active: Boolean,
     ridesCount: Number,
