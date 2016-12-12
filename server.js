ff1b12334f1c196a5409eb3ca7058577fac68ce9
Mendy <mendy@talenttribe.me>
2016-12-12 09:48:20 +0200

    fix route

--- server.js
+++ server.js
@@ -74,8 +74,8 @@ app.use(bodyParser.json());
 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
-//app.use(express.static('public'));
-app.use(express.static('client/public'));
+app.use(express.static('public'));
+//app.use(express.static('client/public'));
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
