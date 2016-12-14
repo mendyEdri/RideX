bd3faa63f8182ce31a24e9cd38eca67cce55f92a
Mendy <mendy@talenttribe.me>
2016-12-14 17:32:12 +0200

    location

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.use('/users', users);
 // });
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    //res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
