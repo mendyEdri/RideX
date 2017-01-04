a5aeb96920054754ef250823e418f5604f906702
Mendy <mendy@talenttribe.me>
2017-01-04 14:46:11 +0200

    button

--- public/app/App.js
+++ public/app/App.js
@@ -420,7 +420,7 @@ class App extends Component {
 							</div>
 							</div>
 						</div>
-						<Button disabled={ this.state.spinKey != i ? true : false } onClick={(event) => this.handleSendDriverClick(i)} key={i} style={rowButton}>
+						<Button disabled={ this.state.spinKey != i ? false : true } onClick={(event) => this.handleSendDriverClick(i)} key={i} style={rowButton}>
 							Order
 						</Button>
 					</div>
